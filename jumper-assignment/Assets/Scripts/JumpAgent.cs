fa88a838a59b074213ebc0ccbfe61a62007ff084
Gil Struyf <gil.struyf@gmail.com>
2023-03-29 13:20:04 +0200

    added a jumperagent script that agent can jump and also added a tag on the floor "Floor"

--- jumper-assignment/Assets/Scripts/JumpAgent.cs
+++ jumper-assignment/Assets/Scripts/JumpAgent.cs
@@ -1,18 +1,84 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+using Google.Protobuf.WellKnownTypes;
+using System;
 
-public class JumpAgent : MonoBehaviour
+
+[RequireComponent(typeof(Rigidbody))]
+public class JumpAgent : Agent
 {
-    // Start is called before the first frame update
+
+    public GameObject obstacle;
+    private float obstacleSpeed = 0;
+
+    Rigidbody rb;
+    bool onGround = true;
+
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+    public override void OnEpisodeBegin()
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override void CollectObservations(VectorSensor sensor)
+    {    // Agent positie    
+        sensor.AddObservation(this.transform.localPosition.y);
+
+        sensor.AddObservation(obstacleSpeed);
     }
+
+    public float jumpForce = 30f;
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {    // Acties, size = 2;
+
+        bool jump = actionBuffers.DiscreteActions[0] >= 1;
+
+        if (jump & onGround) // if jump button is pressed and is on the ground
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            onGround = false;
+        }
+
+       
+
+
+    }
+
+
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var DiscreteActionsOut = actionsOut.DiscreteActions;
+
+
+        DiscreteActionsOut[0] = Mathf.RoundToInt(Input.GetAxis("Vertical"));
+        print(DiscreteActionsOut[0]);
+    }
+
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            onGround = true;
+        }
+
+
+
+    }
+
+
+
+
+
+
 }
